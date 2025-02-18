7590f8b703416160cc9b5944a57f5905a7790b2b
Nicolas Bisson <nicolasbisson14@gmail.com>
2025-02-17 22:21:28 -0500

    Handle multiple cards

--- src/gameplay/Card/card_manager.gd
+++ src/gameplay/Card/card_manager.gd
@@ -1,4 +1,60 @@
 extends Node
 
+signal on_start_hover_card(card : Card)
+signal on_end_hover_card(card : Card)
 signal on_prepare_card(card : CardData)
 signal on_play_card(card : CardData)
+
+var current_card: Card
+var hovered_cards: Array[Card]
+
+func _ready() -> void:
+    on_start_hover_card.connect(start_hover_card)
+    on_end_hover_card.connect(end_hover_card)
+    
+func _process(delta: float) -> void:
+    if current_card == null:
+        return
+        
+    match current_card.current_state:
+        Card.State.HOVERED:
+            update_hovered_card()
+
+func update_hovered_card() -> void:
+    if hovered_cards.is_empty():
+        return
+    elif hovered_cards.size() == 1:
+        if current_card == null:
+            current_card = hovered_cards[0]
+            current_card.set_state(Card.State.HOVERED)
+        return
+    
+    var mouse_position: Vector2 = get_viewport().get_mouse_position()
+    var shortest_distance: float = 1000
+    var new_card: Card
+    for card: Card in hovered_cards:
+        var distance: float = abs(card.global_position.x - mouse_position.x)
+        if distance < shortest_distance:
+            shortest_distance = distance
+            new_card = card
+    if (new_card != current_card):
+        current_card.set_state(Card.State.NONE)
+        current_card = new_card
+        current_card.set_state(Card.State.HOVERED)
+    
+func start_hover_card(card: Card) -> void:
+    if card.current_state != Card.State.NONE:
+        return
+
+    hovered_cards.append(card)
+    update_hovered_card()
+    
+func end_hover_card(card: Card) -> void:
+    hovered_cards.erase(card)
+    if current_card.current_state != Card.State.HOVERED:
+        return
+        
+    if card == current_card:
+        current_card.set_state(Card.State.NONE)
+        current_card = null
+        update_hovered_card()
--- src/ui/components/card/card.gd
+++ src/ui/components/card/card.gd
@@ -2,6 +2,13 @@ class_name Card
 
 extends Area2D
 
+enum State{
+    NONE,
+    HOVERED,
+    GRABBED,
+    SELECTED,
+}
+
 @export var data: CardData
 @export var title_path: String = "Background/Title"
 @export var description_path: String = "Background/Description"
@@ -9,16 +16,16 @@ extends Area2D
 @onready var title: Label = get_node(description_path)
 @onready var description: Label = get_node(description_path)
 
+var current_state: State = State.NONE
+
 func _ready() -> void:
     set_text_from_data()
 
 func _on_mouse_entered() -> void:
-    scale = Vector2(1.05, 1.05)
-    position += Vector2(0, -200)
+    CardManager.on_start_hover_card.emit(self)
 
 func _on_mouse_exited() -> void:
-    scale = Vector2(1.05, 1.05)
-    position += Vector2(0, 200)
+    CardManager.on_end_hover_card.emit(self)
     
 func set_text_from_data() -> void:
     title.text = data.name
@@ -34,5 +41,19 @@ func set_text_from_data() -> void:
         text += str(min) + "-" + str(max) + " " + Dices.Type.find_key(dice.type) + "\n"
     
     description.text = text
-    
+
+func set_state(new_state: State) -> void:
+    match new_state:
+        State.NONE:
+            scale = Vector2(1, 1)
+        State.HOVERED:
+            scale = Vector2(1.05, 1.05)
+        State.GRABBED:
+            scale = Vector2(1, 1)
+        State.SELECTED:
+            scale = Vector2(1.2, 1.2)
+        _:
+            return
+            
+    current_state = new_state
     
